af0849d0f54ef1a637514b32532c1eed0ba18c4f
Rizki Ningrat <zeinforrell69@gmail.com>
2023-12-10 22:19:13 +0700

    adding style

--- autorun.bat
+++ autorun.bat
@@ -1,13 +1,15 @@
 @echo off
+color 02
 setlocal EnableDelayedExpansion
 title autorunserver
+mode con: cols=80 lines=20
 
 :menu
 cls
 echo AutoRunServer
 echo --------------------
-echo 1. Start from zero (create a new venv and install all requirements)
-echo 2. Start Django (Assume Django and any other libraries are already installed)
+echo 1. Start from zero (Create a new venv and install all requirements)
+echo 2. Start Django (Assume Django is already installed in your system)
 echo 3. Exit
 echo.
 set /p choice="Your choice: "
@@ -20,15 +22,16 @@ goto menu
 :A
 REM pindah directory
 cd %~dp0
-REM membuat virtual environment
+echo.
+echo Creating virtual environment
 python -m venv .venv
-REM mengaktifkan venv
+REM Mengaktifkan venv
 cd .venv/Scripts/
 call Activate.bat
 cd ../../
-pause
-REM menginstal beberapa library yang diperlukan
-pip install -r requirements.txt
+echo Installing requirements, please wait..
+echo This process will take 2 or 3 minutes
+pip install -r requirements.txt > output.log 2>&1
 
 :B
 echo.
@@ -37,13 +40,13 @@ set lib[1].name=requests
 set lib[2].name=mysqlclient         
 set lib[3].name=djangorestframework 
 REM memastikan library yang dibutuhkan terinstall
-echo Checking requirements..
+echo Checking all requirements..
 for /L %%A in (0 1 3) do (
     call pip show %%lib[%%A].name%% > nul
 
     if errorlevel 1 (
         echo Installing, please wait..
-        call pip install %%lib[%%A].name%%
+        call pip install %%lib[%%A].name%% > output.log 2>&1
     ) else (
         call echo Library %%lib[%%A].name%% installed
     )
@@ -64,8 +67,9 @@ REM mulai django
 echo.
 echo Starting Django, please wait..
 cd %~dp0/fast_print
-python manage.py makemigrations
-python manage.py migrate
-python manage.py runserver
+python manage.py makemigrations > ouput.log 2>&1
+python manage.py migrate > ouput.log 2>&1
+start http://127.0.0.1:8000/
+python manage.py runserver > ouput.log 2>&1
 
 endlocal
